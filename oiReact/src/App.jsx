c62a17263dbff3be7b074a6e5af9f56c7811f741
Manuel-Gon <manuel_gonzalez@estudante.sesisenai.org.br>
2024-08-23 17:16:13 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Dinossauros</h1>
+      <h2>Carnivoros</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+         Asperiores, molestiae ipsam? Totam, natus unde. At ipsum quasi beatae hic repellendus, 
+         laborum velit voluptates aliquid atque inventore voluptate labore illum ab?
+
+      </p>
+    </>
+  )
+}
+
+export default App
